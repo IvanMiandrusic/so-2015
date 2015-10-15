a0317faf73e6247f7b054df7e886458b44e5dc44
fcatinello <fcatinello@gmail.com>
2015-10-15 00:40:21 -0300

    Arreglo tema path relativos y absolutos

--- Planificador/src/Utils.c
+++ Planificador/src/Utils.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <commons/string.h>
+#include <limits.h>
 #include "Utils.h"
 
 char* convertToString(int32_t num) {
@@ -75,52 +76,16 @@ int32_t get_actual_time_integer() {
 
 char* generate_absolute_path(char* filePath) {
 
-	/** Creo el comando locate para encontrar el path del archivo en el FS y redirecciono salida en archivo **/
-	char* command = string_new();
-	string_append_with_format(&command, "locate -i -l 1 %s > locate.out", filePath);
+	char absolute_path[PATH_MAX+1];
 
-	/** Ejecuto el comando **/
-	system(command);
+	char* found = realpath(filePath, absolute_path);
 
-	/** Abro archivo para leer el path **/
-	FILE* output_file = fopen("locate.out", "r");
-	if(NULL == output_file)
-	{
-		log_error(loggerError, ANSI_COLOR_BOLDRED "Un error ocurrio al abrir el archivo con el path" ANSI_COLOR_RESET);
-		printf(ANSI_COLOR_BOLDRED "Un error ocurrio al abrir el archivo con el path" ANSI_COLOR_RESET);
-		return NULL;
-	}
-
-	/** Obtengo el tamaño del contenido **/
-	fseek(output_file, 0, SEEK_END);
-	int32_t fsize = ftell(output_file);
-	if(fsize < 0) {
-		log_error(loggerError, ANSI_COLOR_BOLDRED "Un error ocurrio para saber tamaño del archivo con el path" ANSI_COLOR_RESET);
-		printf(ANSI_COLOR_BOLDRED "Un error ocurrio para saber tamaño del archivo con el path" ANSI_COLOR_RESET);
-		return NULL;
-	}
-	fseek(output_file, 0, SEEK_SET);
-
-	/** Leo el contenido (el path absoluto) **/
-	char* absolute_path = malloc(fsize);
-	int32_t read = fread(absolute_path, fsize, 1, output_file);
-	if(read == 0) {
-		log_error(loggerError, ANSI_COLOR_BOLDRED "Un error ocurrio al leer el archivo con el path" ANSI_COLOR_RESET);
-		printf(ANSI_COLOR_BOLDRED "El path del mCod ingresado es incorrecto" ANSI_COLOR_RESET);
-		return NULL;
-	}
-
-	/** Cierro el archivo **/
-	if(!fclose(output_file)){
-		log_error(loggerError, ANSI_COLOR_BOLDGREEN "Exito al cerrar el archivo con el path" ANSI_COLOR_RESET);
+	/** Chequeo si encontro el archivo en FS y obtengo el path absoluto **/
+	if(found != NULL) {
+		return absolute_path;
 	}
 	else {
-		log_error(loggerError, ANSI_COLOR_BOLDRED "Un error ocurrio al cerrar el archivo con el path" ANSI_COLOR_RESET);
-		printf(ANSI_COLOR_BOLDRED "Un error ocurrio al cerrar el archivo con el path" ANSI_COLOR_RESET);
+		perror(ANSI_COLOR_BOLDRED "The mCod no fue encontrado" ANSI_COLOR_RESET);
 		return NULL;
 	}
-
-	absolute_path[fsize-1] = '\0';
-
-	return absolute_path;
 }
