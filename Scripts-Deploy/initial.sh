a187afabdab5d0944a90f20b709085ee9d83c402
Federico <fcatinello@gmail.com>
2015-11-20 00:34:32 -0300

    Correct initial script

--- Scripts-Deploy/initial.sh
+++ Scripts-Deploy/initial.sh
@@ -6,13 +6,14 @@ clear
 cd /home/utnso/
 echo "export LD_LIBRARY_PATH=/home/utnso/tp-2015-2c-elclan/lib" >  /home/utnso/shared_library
 
-cd /home/utnso/tp-2015-2c-elclan/lib
-make all
-
 cd /home/utnso/tp-2015-2c-elclan/Scripts-Deploy
 
 chmod +x commons-setup.sh
 ./commons-setup.sh
+
+cd /home/utnso/tp-2015-2c-elclan/lib
+make all
+
 clear
 
 exit 0
